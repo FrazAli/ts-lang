de1655442934ece0584e3e23c184d4bd90a92408
Fraz Ali <frazaliqureshi@gmail.com>
2022-11-13 21:37:04 +0100

    course: interface example

--- importjson/src/index.ts
+++ importjson/src/index.ts
@@ -1,8 +1,21 @@
 import axios from 'axios';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 const url = 'http://jsonplaceholder.typicode.com/todos/1';
 
 axios.get(url).then(response => {
-  console.log(response.data);
+  const todo: Todo = response.data as Todo;
+  console.log(`
+    ${todo.id},
+    ${todo.title},
+    ${todo.completed}
+  `);
+
+  // console.log(response.data);
 });
 
